dda640d64c8b3eab7c6e5a2819c8b42565c3bd44
urtokk <xendios@gmail.com>
2023-05-06 17:39:42 +0200

    test(config): :white_check_mark: added rudimentary test to load config

--- src/config.rs
+++ src/config.rs
@@ -32,3 +32,41 @@ impl AwsSetterConfig {
         self.profiles.keys().collect()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_load_config() {
+        let config = AwsSetterConfig::load("resources/test_config.toml").unwrap();
+        assert_eq!(config.email, "test@example.com");
+        assert_eq!(config.profiles.len(), 2);
+        assert_eq!(config.get_role("default"), Some(&"arn:aws:iam::123456789012:role/MyRole".to_string()));
+    }
+
+    #[test]
+    fn test_get_role() {
+        let config = AwsSetterConfig {
+            email: "test@example.com".to_string(),
+            profiles: [("default".to_string(), "arn:aws:iam::123456789012:role/MyRole".to_string())]
+                .iter().cloned().collect(),
+        };
+        assert_eq!(config.get_role("default"), Some(&"arn:aws:iam::123456789012:role/MyRole".to_string()));
+        assert_eq!(config.get_role("nonexistent"), None);
+    }
+
+    #[test]
+    fn test_list_profiles() {
+        let config = AwsSetterConfig {
+            email: "test@example.com".to_string(),
+            profiles: [("default".to_string(), "arn:aws:iam::123456789012:role/MyRole".to_string()),
+                       ("other".to_string(), "arn:aws:iam::123456789012:role/OtherRole".to_string())]
+                .iter().cloned().collect(),
+        };
+        let profiles = config.list_profiles();
+        assert_eq!(profiles.len(), 2);
+        assert!(profiles.contains(&&"default".to_string()));
+        assert!(profiles.contains(&&"other".to_string()));
+    }
+}
